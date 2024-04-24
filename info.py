d76173b823c770b88cf3c91c6c53e4510b42656b
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2024-04-24 12:48:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 API_ID       = 28280795
 API_HASH     = "3f02c2df6069de8c1a2abf623da0a4b8"
-BOT_TOKEN    = "6576609270:AAGLAbzgaVGfrr5z2xWXA-gC_fngjA-Krkc"
+BOT_TOKEN    = "6537674730:AAHsRA9ViM0xYZPDWf-zbWljxRwKK6zIEB4"
 SESSION      = "BQF4fgoAv0yrMlgiSnpJgm_jTBW8Tx8k9I5oNNJ8q-Ro9LTKVFghdKqoQayZ_aCNr2bjfqH-3KTJfY2wpV-pgqMbgBxG0_zAQLqPGvvV2QVuySnl5r-7PAbdrHs687xrKkfXHDjNEzy6fgy8_PkaOQiY-SjE9TAkGSis3zTt8ohA3Y66kr6MqCov-FFZQ4BDydtyfZW5IufdYKNPfff-BqdxszO2NSSQar7PUtyacEaU62OmI2dtixg88fDy1w-r_J_Z924ot8SPyLr3RtBNaADCSX6VhPLtEo1Bm4X9r8bPgvr9VQ7EGLEsMtKKqhTiid2OXYYkIJromBkDABwXdhkXzT2f_gAAAAF1aAloAA"
 DATABASE_URI = "mongodb+srv://Boobs:0@cluster0.bmheo5h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 LOG_CHANNEL  = -1001961336094
