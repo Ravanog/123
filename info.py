c629fe6dd222fe5f1f8d57639a4a2804bcca6f1e
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2024-04-24 22:19:08 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,13 +1,13 @@
-API_ID       = 28280795
-API_HASH     = "3f02c2df6069de8c1a2abf623da0a4b8"
-BOT_TOKEN    = "6537674730:AAHsRA9ViM0xYZPDWf-zbWljxRwKK6zIEB4"
+API_ID       = 22971260
+API_HASH     = "39b93b45f525642b9c732f43185e4ab0"
+BOT_TOKEN    = "6774405476:AAHB-RMY8GDJHhLDAHm68y4H3VhYVgmn3HA"
 SESSION      = "BQF4fgoAO-DZi0X5UxAuaKuc2a-OfcHb96jDgr12HDu_mnv3mBWoa6WaRlBSfXstSGtPAD1Wqf5lrvjIa1Km0wEjtF3etRZ5JPJiilROa6vlxbHbDweRS0Y35hl3G95DTuMw7985A6a22jYgGARul7-4MU9HzFrOiQQuOctKg0FMdvsIwZJDD8B3ugMgVX3kMpkiSfcOnevgYwU0-QkJGZxTBJ076SE5LZh_UcPUJFPjNExXw7-z4wtGxtUavrkWI-nTb1WcQqnvxVi_p2Z8q5MrGRnnO_JCQtuwcOjBCH5I_JKTPxw9cconTXchShTOL7ngKgO9-Ogf1hcn34cw7SGHDqNmcgAAAAF1aAloAA"
 DATABASE_URI = "mongodb+srv://Boobs:0@cluster0.bmheo5h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = -1001961336094
-ADMIN        = 6651109872
-CHANNEL      = "-1001961336094"
+LOG_CHANNEL  = 1002039419270
+ADMIN        = 5911514776
+CHANNEL      = "1002039419270"
 APPROVETEXT  = "Hᴇʟʟᴏ {mention}\nWᴇʟᴄᴏᴍᴇ Tᴏ {title}\n\nYᴏᴜʀ ʀᴇǫᴜᴇsᴛ ʜᴀs ʙᴇᴇɴ ᴀᴘᴘʀᴏᴠᴇᴅ"
 APPROVE     = "on"
-OWNER_USERNAME = "ROYAL_GAGAN_DEEP_SINGH_MEHRA"
+OWNER_USERNAME = "KUSHALHK"
 UPI_PIC = "https://telegra.ph/file/e28b3c353c24706fb67d8.jpg"
-BOT_USERNAME = "Movies_villa_post_search_bot"
+BOT_USERNAME = "Post_Search_HK_bot"
