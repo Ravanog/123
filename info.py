ce8f8a472a93076ed10c7e16c80dda7751a465a2
Preet mehra <143023174+ROYAL-JATT@users.noreply.github.com>
2024-04-24 22:28:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,9 +3,9 @@ API_HASH     = "39b93b45f525642b9c732f43185e4ab0"
 BOT_TOKEN    = "6774405476:AAHB-RMY8GDJHhLDAHm68y4H3VhYVgmn3HA"
 SESSION      = "BQF4fgoAO-DZi0X5UxAuaKuc2a-OfcHb96jDgr12HDu_mnv3mBWoa6WaRlBSfXstSGtPAD1Wqf5lrvjIa1Km0wEjtF3etRZ5JPJiilROa6vlxbHbDweRS0Y35hl3G95DTuMw7985A6a22jYgGARul7-4MU9HzFrOiQQuOctKg0FMdvsIwZJDD8B3ugMgVX3kMpkiSfcOnevgYwU0-QkJGZxTBJ076SE5LZh_UcPUJFPjNExXw7-z4wtGxtUavrkWI-nTb1WcQqnvxVi_p2Z8q5MrGRnnO_JCQtuwcOjBCH5I_JKTPxw9cconTXchShTOL7ngKgO9-Ogf1hcn34cw7SGHDqNmcgAAAAF1aAloAA"
 DATABASE_URI = "mongodb+srv://Boobs:0@cluster0.bmheo5h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-LOG_CHANNEL  = 1002039419270
+LOG_CHANNEL  = -1002039419270
 ADMIN        = 5911514776
-CHANNEL      = "1002039419270"
+CHANNEL      = "-1002039419270"
 APPROVETEXT  = "Hᴇʟʟᴏ {mention}\nWᴇʟᴄᴏᴍᴇ Tᴏ {title}\n\nYᴏᴜʀ ʀᴇǫᴜᴇsᴛ ʜᴀs ʙᴇᴇɴ ᴀᴘᴘʀᴏᴠᴇᴅ"
 APPROVE     = "on"
 OWNER_USERNAME = "KUSHALHK"
